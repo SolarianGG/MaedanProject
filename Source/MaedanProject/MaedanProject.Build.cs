903efc870d77f2ae7dc2b0f6989a6cc98343d998
Matsvei Piarerva <pererva.matvel@gmail.com>
2025-03-10 19:53:53 +0900

    Changed basic render and platform support project settings
    - Changed global illumination method to Screen Space
    - Changed reflection method to Screen Space
    - Disabled Nanite

--- Source/MaedanProject/MaedanProject.Build.cs
+++ Source/MaedanProject/MaedanProject.Build.cs
@@ -8,9 +8,14 @@ public class MaedanProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PublicIncludePaths.AddRange(new string[]
+		{
+			// TODO: Add some include stuff
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
